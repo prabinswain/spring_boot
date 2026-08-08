9d945e90fd514bd3b9f054d2b51f752cd0c1f7cd
Prabin Swain <prabinswain027@gmail.com>
2026-08-08 20:16:09 +0530

    basic project

---
+++ Spring_SpringBootScratch/basic-projects/src/test/java/com/basic/BasicProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
